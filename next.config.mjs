6a5519b2441bff507ea9d05b6fb3b865b21f5885
Ahmed Samir <ahmedsamir18824@gmail.com>
2024-10-19 16:06:33 +0300

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  basePath: "/Next-Posts",
   images: {
     remotePatterns: [{ hostname: "res.cloudinary.com" }],
   },
